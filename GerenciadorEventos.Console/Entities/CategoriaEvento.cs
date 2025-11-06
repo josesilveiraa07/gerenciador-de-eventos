a3254b900ffd8bcd95572c5068280bc99c9294aa
Jose Silveira <jose@hubii.com.br>
2025-11-06 20:20:12 -0300

    feat: add projet to sln

---
+++ GerenciadorEventos.Console/Entities/CategoriaEvento.cs
@@ -0,0 +1,8 @@
+namespace DefaultNamespace;
+
+public class CategoriaEvento
+{
+    private int id;
+    private string nome;
+    private string descricao;
+}
---
+++ GerenciadorEventos.Console/Entities/Evento.cs
@@ -0,0 +1,9 @@
+using DefaultNamespace;
+
+public class Evento
+{
+  private int id;
+  private string descricao;
+  private DateTime data;
+  private CategoriaEvento categoriaEvento;
+}
